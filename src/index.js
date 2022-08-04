87120339ab432aebb3eb0967f75a98da9ae9e6c3
Lucas Lafèré <laferelucas@gmail.com>
2022-08-04 12:07:57 -0300

    feat: added all routes structure, no architecture yet

--- src/index.js
+++ src/index.js
@@ -7,5 +7,30 @@ const app = express();
 app.use(cors());
 app.use(json());
 
+app.post("/sign-up", (req, res) => {
+    
+})
+app.post("/sign-in", (req, res) => {
+
+})
+app.post("/urls/shorten", (req, res) => {
+
+})
+app.get("/urls/:id", (req, res) => {
+
+})
+app.get("/urls/open/:shortUrl", (req, res) => {
+
+})
+app.delete("/urls/:id", (req, res) => {
+
+})
+app.get("/users/me", (req, res) => {
+
+})
+app.get("/ranking", (req, res) => {
+
+})
+
 
 app.listen(process.env.PORT, () => console.log("Rodando"))
