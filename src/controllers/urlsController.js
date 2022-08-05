32b09bf9362a53095b6a7824df3b63d9135dc3b7
Lucas Lafèré <laferelucas@gmail.com>
2022-08-05 13:36:21 -0300

    feat: post/urls/shorten properly works, fully functional

--- src/controllers/urlsController.js
+++ src/controllers/urlsController.js
@@ -1,5 +1,6 @@
 import connection from '../db/database.js';
 import { urlValidation } from '../schemas/urlSchema.js';
+import { nanoid } from 'nanoid';
 
 export async function shortenUrl (req, res) {
     const {userId} = res.locals;
@@ -7,16 +8,28 @@ export async function shortenUrl (req, res) {
     const { error } = urlValidation.validate(url);
     if (!url || error) return res.status(422).send("Please input a valid url");
     try {
-        
+        const shortUrl = nanoid();
+        if (!shortUrl) return res.status(400).send("There was an error shortening this url");
         const insertUrl = await connection.query(`
         INSERT INTO urls
-        (url)
-        VALUES ($1);
-        `, [url.url]);
+        (url, "userId")
+        VALUES ($1, $2);
+        `, [url.url, userId]);
+        const urlId = await connection.query(`
+        SELECT id FROM urls
+        WHERE url = $1 AND "userId" = $2;
+        `, [url.url, userId]);
+        const insertShortUrl = await connection.query(`
+        INSERT INTO "shortUrls"
+        (identifier, "urlId", "userId")
+        VALUES ($1, $2, $3);
+        `, [shortUrl, urlId.rows[0].id, userId]);
 
-    return res.status(201).send("funfou");
+    return res.status(201).json({
+        shortUrl
+    });
     } catch (error) {
-        
+        return res.sendStatus(500);
     } 
 }
 
